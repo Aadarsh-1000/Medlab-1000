d5d41e0b05bb94589fcb6a901e88626a1d8642d9
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-09 15:43:34 +0530

    added api

--- api/generate.js
+++ api/generate.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
     return;
   }
 
-  const apiKey = process.env.GEMINI_API_KEY;
-  console.log("ENV TEST:", process.env.GEMINI_API_KEY);
+  const apiKey = process.env.GROQ_API_KEY;
+  console.log("ENV TEST:", process.env.GROQ_API_KEY);
 console.log("API EXISTS:", !!apiKey);
   if (!apiKey) {
     res.status(200).json({
