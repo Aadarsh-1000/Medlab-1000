0dd9514e8d59cf9cd0ab5d72f5b2f49c230b8888
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-18 15:26:34 +0530

    chnaged db file path into public

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -1,15 +1,22 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "path";
+import fs from "fs";
 
 export async function getDB() {
 
   const dbPath = path.join(
     process.cwd(),
+    "public",
     "combined_medical_database_optimized.db"
   );
 
   console.log("DB PATH:", dbPath);
+
+  console.log(
+    "DB EXISTS:",
+    fs.existsSync(dbPath)
+  );
 
   return open({
     filename: dbPath,
